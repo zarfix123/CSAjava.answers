58b0b2901c9e8412e6c059a8ffd88cd430b8c857
zarfix123 <zarfix.42@gmail.com>
2023-10-26 12:59:56 -0700

    began unit 2

---
+++ theAnswers/Unit 2/2.1.9 Game Skeleton.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * The ConsoleGame class represents a console game.
+ * 
+ * Add the instance variables you think a console game would need
+ */ 
+
+public class ConsoleGame
+{
+
+    // Attributes
+    private String system;
+    private int storagespace;
+    private double cost;
+   
+    
+
+}
